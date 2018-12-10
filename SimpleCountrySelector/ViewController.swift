4482644fe6bb6c2cab7082b0c053f92ef6ab56b9
Mohamed Abdul-Hameed <mhd-abdulhameed@mohameds-macbook-pro-2.local>
2018-12-10 21:21:35 +0200

    Initial Commit

---
+++ SimpleCountrySelector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleCountrySelector
+//
+//  Created by Mohamed Abdul-Hameed on 12/10/18.
+//  Copyright © 2018 Mohamed Abdul-Hameed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
